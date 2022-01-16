a03df863fb6cda7e67a4648a3890cf34d8cb53b1
Martin van Velsen <vvelsen@gmail.com>
2022-01-16 09:07:18 -0500

    Reworked the build scripts to use the sanme naming conventions as the rest

--- src/server/run-build.sh
+++ src/server/run-build.sh
@@ -1,7 +1,7 @@
 clear
 
 if [ ! -d "node_modules" ]; then
-  echo "Error: no node_modules folder found, please execute 'run-prep.sh first"
+  echo "Error: no node_modules folder found, please execute 'run-install.sh first"
   exit
 fi
 
